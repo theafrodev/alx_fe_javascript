253673c7dea10a565a4b7abf1b2f0d1a527487ef
theafrodev <fialorchristian@gmail.com>
2024-07-28 10:48:35 +0000

    assignment 2

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -16,19 +16,19 @@ let script = document.querySelector('script');
 let quotes = [
     {
         text: 'this is a quote',
-        category: 'this is a quote category 1'
+        category: 'philosophy'
     },
     {
         text: 'this is a quote 2',
-        category: 'this is a quote category 2'
+        category: 'wellbeing'
     },
     {
         text: 'this is a quote 3',
-        category: 'this is a quote category 3'
+        category: 'roots'
     },
     {
         text: 'this is a quote 4',
-        category: 'this is a quote category 4'
+        category: 'wellbeing'
     }
 ]
 
@@ -64,16 +64,26 @@ function createAddQuoteForm(){
 
 function addQuote(){
     let newQuotetext = document.getElementById('newQuoteText').value.trim();
-
     let newQuoteCategory = document.getElementById('newQuoteCategory').value.trim();
    
     quotes.push({text: newQuotetext, category: newQuoteCategory});
 
     let addedText = document.createElement('div');
-
     addedText.textContent = `${newQuotetext} added successfully`;
-
     quoteDisplay.appendChild(addedText);
+
+    let categoryExists = false;
+    for (let i = 0; i < categoryFilter.options.length; i++) {
+        if (categoryFilter.options[i].value === newQuoteCategory) {
+            categoryExists = true;
+            break;
+        }
+    }
+
+    // If the category does not exist, add it to the dropdown
+    if (!categoryExists) {
+        categoryFilter.insertAdjacentHTML("beforeend", `<option value="${newQuoteCategory}">${newQuoteCategory}</option>`);
+    }
 
     // let quoteStorage = JSON.stringify(quotes);
 
@@ -86,14 +96,16 @@ function getQuotes(){
 
     if (localStorage.getItem('quotes')){
         // console.log(localStorage.getItem('quotes'));
+        return localStorage.getItem('quotes');
     }else{
         saveQuotes();
         // console.log(localStorage.getItem('quotes'));
+        return localStorage.getItem('quotes');
     }
 
-    console.log(JSON.parse(localStorage.getItem('quotes')));
+    // console.log(JSON.parse(localStorage.getItem('quotes')));
 
-    return localStorage.getItem('quotes');
+    // return localStorage.getItem('quotes');
 
 }
 
@@ -140,8 +152,63 @@ function importFromJsonFile(event) {
     //console.log(quotes);
 
     fileReader.readAsText(event.target.files[0]);
+  } 
+  
+  let categoryFilter = document.getElementById('categoryFilter');
+
+  function populateCategories(){
+
+    let categories = [];
+    
+    quotes.forEach((item)=>{
+        categories.push(item.category);
+    });
+
+    categories = [...new Set(categories)]
+
+    console.log('Categories:' + categories);
+
+    categories.forEach((item)=>{
+        categoryFilter.insertAdjacentHTML("beforeend",`<option value=${item}>${item}</option>`);
+    });
+
   }
 
+  function filterQuotes(option){
+
+    quoteDisplay.innerHTML = '';
+
+    quotes.forEach((item)=>{
+        if(item.category === option){
+            quoteDisplay.insertAdjacentHTML("beforeend", `<h6>${item.text}</h6>`);
+            localStorage.setItem('category', `${item.category}`);
+        }else if(option === "All Categories"){
+            quoteDisplay.insertAdjacentHTML("beforeend", `<h6>${item.text}</h6>`);
+            localStorage.setItem('category', `${option}`);
+        }
+    });
+  }
+
+  function getFilter(){
+
+    quoteDisplay.innerHTML = '';
+
+    let option = localStorage.getItem('category');
+
+    categoryFilter.value = option;
+
+    console.log(option + 'here');
+
+    quotes.forEach((item)=>{
+        if(option === "All Categories"){
+            quoteDisplay.insertAdjacentHTML("beforeend", `<h6>${item.text}</h6>`);
+        }
+        else if(item.category === option){
+            quoteDisplay.insertAdjacentHTML("beforeend", `<h6>${item.text}</h6>`);
+        }
+    });
+
+  }
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
@@ -149,10 +216,25 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     showRandomQuotes();
 
+    populateCategories();
+
     // console.log(body);
 
     newQuote.addEventListener('click', ()=>{showRandomQuotes()});
 
     createAddQuoteForm();
+
+    categoryFilter.addEventListener('change', () => {
+        const selectedValue = categoryFilter.value;
+        console.log('Selected value:', selectedValue);
+
+        const selectedIndex = categoryFilter.selectedIndex;
+        const selectedOption = categoryFilter.options[selectedIndex];
+        console.log('Selected text:', selectedOption.text);
+
+        filterQuotes(selectedOption.text);
+    });
+
+    getFilter(); 
 
 });
